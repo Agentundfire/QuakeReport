bbfaeac80a5d9a955b92e4d14097c6b861fc18de
RJ <201213388@ingenieria.usac.edu.gt>
2017-05-16 08:23:30 -0600

    Set up SettingsActivity and Settings button in app bar

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -24,6 +24,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -37,9 +39,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     //Global EarthquakeAdapter of earthquakes
     private EarthquakeAdapter mAdapter;
 
-    /**
-     * TextView that is displayed when the list is empty
-     */
+    // TextView that is displayed when the list is empty
     private TextView mEmptyStateTextView;
 
     private static final int EARTHQUAKE_LOADER_ID = 1;
@@ -131,6 +131,23 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     public void onLoaderReset(Loader<ArrayList<Earthquake>> loader) {
         // Loader reset, so we can clear out our existing data.
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
